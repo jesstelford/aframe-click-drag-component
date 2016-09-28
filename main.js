25efc1cef983243cee774b83a4e09f8c144ec7a8
Jess Telford <jess+github@jes.st>
2016-09-28 22:54:29 +1000

    Updates

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import aframe from 'aframe';
+import extras from 'aframe-extras';
 import clickDragComponent from '../src/index';
+
+extras.physics.registerAll(aframe);
 clickDragComponent(aframe);
+
